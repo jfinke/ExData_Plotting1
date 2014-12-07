75ee91a694e4b878cc10f1d08858260f53e376a2
Jeremy Finke <jeremy.finke@gmail.com>
2014-12-07 15:25:09 -0600

    plot2.R
    copied over contents of plot1

---
+++ plot2.R
@@ -0,0 +1,25 @@
+# Read in the source data file
+
+hpc <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
+
+# Add a new column in date / time based on the Date & Time columns
+hpc$DateTime<-strptime(paste(hpc$Date, hpc$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+# Selecting the relevant data for the exercise
+hpc2<-with(hpc, subset(hpc, hpc$DateTime >= as.POSIXct('2007-02-01 00:00:00')))
+hpc3<-with(hpc2, subset(hpc2, hpc2$DateTime < as.POSIXct('2007-02-03 00:00:00')))
+
+
+# Opening a png device to write the file
+png(filename = "plot2.png", width = 480, height = 480)
+
+# Drawing the histogram
+hist(hpc3$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+
+# Closing the png device
+dev.off()
+
+
+
+
+
