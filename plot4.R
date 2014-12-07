7a1ef919e2beddc90f03cfa0939312acf587f1a7
Jeremy Finke <jeremy.finke@gmail.com>
2014-12-07 16:53:57 -0600

    Plot4 done
    Plot4.R finalized, plot4.png created.

--- plot4.R
+++ plot4.R
@@ -17,15 +17,25 @@ png(filename = "plot4.png", width = 480, height = 480)
 #hist(hpc3$Global_active_power, main = "Global Active Power", 
 #     xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 
-# Drawing the plot
-#plot(hpc3$DateTime, hpc3$Global_active_power, type="l", xlab = "" ,ylab = "Global Active Power (kilowatts)")
+# Drawing the plots
+par(mfrow=c(2,2)) # 2x2 matrix of plots
+
+#plot #1
+plot(hpc3$DateTime, hpc3$Global_active_power, type="l", xlab = "" ,ylab = "Global Active Power")
+
+#plot #2
+plot(hpc3$DateTime, hpc3$Voltage, type="l", xlab = "datetime" ,ylab = "Voltage")
+
+#plot #3
 plot(hpc3$DateTime, hpc3$Sub_metering_1, type="l", xlab = "" ,ylab = "Energy sub metering")
 par(new="T")
 plot(hpc3$DateTime, hpc3$Sub_metering_2, type="l", col = "red", ylim=c(0,40), xlab="", ylab="", yaxt="n")
 par(new="T")
 plot(hpc3$DateTime, hpc3$Sub_metering_3, type="l", col = "blue", ylim=c(0,40), xlab="", ylab="", yaxt="n")
-legend("topright", lty=c(1,1),col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", bty = "n", lty=c(1,1),col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+#plot #4
+plot(hpc3$DateTime, hpc3$Global_reactive_power, type="l", xlab = "datetime" ,ylab = "Global_reactive_power")
 
 # Closing the png device
 dev.off()
